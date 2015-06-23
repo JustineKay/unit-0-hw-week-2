8d4139a4b5fbee6c81ff725782c8a07f159719e1
Justine Gartner <GartnerJustine@gmail.com>
2015-06-23 19:17:43 -0400

    First Commit

--- Person/Person/main.m
+++ Person/Person/main.m
@@ -8,6 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
+//Write a method called checkSameCity which accepts one parameter of type Person * and checks if they live in the same city. The method should return a boolean value.
+//A Person has recently had a child, whose name is 'Abc'. Write a method called registerChild which takes 0 parameters and returns a new Person * instance represeting the child, which has the same phoneNumber and city as the parent.
+
+
 @interface Person: NSObject
 
 - (void)setName:(NSString *)name;
@@ -18,6 +22,8 @@
 
 - (void)setPhoneNumber:(NSString *)phoneNumber;
 - (NSString *)phoneNumber;
+
+- (BOOL)checkSameCity:(NSString *)city;
 
 @end
 
@@ -51,13 +57,39 @@
     return _phoneNumber;
 }
 
-@end
+- (BOOL)checkSameCity:(NSString *)city {
+    return [_city isEqualToString:city];
+    
+}
 
+@end
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        Person *julia = [[Person alloc]init];
+        
+        [julia setName:@"Julia"];
+        [julia setCity:@"Cheyenne"];
+        [julia setPhoneNumber:@"617-890-4563"];
+        
+        NSLog(@"%@ lives in %@. Her phone number is %@.", [julia name], [julia city], [julia phoneNumber]);
+        
+        Person *igor = [[Person alloc]init];
+        
+        [igor setName:@"Igor"];
+        [igor setCity:@"Cheyenne"];
+        [igor setPhoneNumber:@"347-517-2345"];
+        
+        NSLog(@"%@ lives in %@. His phone number is %@.", [igor name], [igor city], [igor phoneNumber]);
+        
+//        [[igor city] isEqualToString:[julia city]];
+        NSLog(@"Do %@ and %@ live in the same city? %hhd.", [julia name], [igor name],[julia checkSameCity:[igor city]]);
+        
+        
+        
+        
     }
     return 0;
 }
