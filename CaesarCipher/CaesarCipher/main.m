b1efed8f0fb0fd424122cb560ca7af2dbad128ed
Justine Gartner <GartnerJustine@gmail.com>
2015-06-24 21:22:23 -0400

    Person and Caeser Cipher complete

--- CaesarCipher/CaesarCipher/main.m
+++ CaesarCipher/CaesarCipher/main.m
@@ -8,10 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+//Being amateur codebreakers, we want to know if two distinct looking ciphers correspond to the same input message. Write a method called codeBreaker, which accepts two cipher strings as paramaters and returns a boolean value which tells us whether they are actually the same input message encoded using two different offsets. hint: the maximum offset is 25
+
+//Example:
+//okmg = "mike", offset 2
+//tprl = "mike", offset 7
+//
+//Both are the same input message, but different offset. Your method would return YES in this case
+
 @interface CaesarCipher : NSObject
+
 
 - (NSString *)encode:(NSString *)string offset:(int)offset;
 - (NSString *)decode:(NSString *)string offset:(int)offset;
+
+-(BOOL)codeBreaker: (NSString *)string with:(NSString *)otherString;
 
 @end
 
@@ -50,11 +61,68 @@
     return [self encode:string offset: (26 - offset)];
 }
 
+
+-(BOOL)codeBreaker: (NSString *)string with:(NSString *)otherString {
+    
+    for (int i = 1; i < 26; i++) {
+        
+        NSString *decodedString = [self decode: string offset: i];
+        //NSLog(@"string:%@, offset:%d", decodedString, i);
+        
+        for (int j = 1; j < 26; j++) {
+            
+            NSString *decodedOtherString = [self decode: otherString offset: j];
+            //NSLog(@"string:%@, offset:%d", decodedOtherString, j);
+            
+            if ([decodedString isEqualToString: decodedOtherString]){
+                NSLog(@"Same input.");
+                return 1;
+            }
+        }
+    }
+    NSLog(@"Input does not match.");
+    return 0;
+}
+
 @end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        
+        CaesarCipher *cipher = [[CaesarCipher alloc] init];
+        
+        
+        //encoding examples
+        
+        NSString *example1 = [cipher encode:@"mike" offset: 2];
+        NSLog(@"%@", example1);
+        
+        NSString *example2 = [cipher encode:@"mike" offset: 7];
+        NSLog(@"%@", example2);
+        
+        NSString *example3 = [cipher encode:@"seat" offset: 7];
+        NSLog(@"%@", example3);
+        
+        //decoding examples
+        
+        NSString *decodedExample1 = [cipher decode:@"okmg" offset: 2];
+        NSLog(@"%@", decodedExample1);
+        
+        NSString *decodedExample2 = [cipher decode: @"tprl" offset:7];
+        NSLog(@"%@",decodedExample2);
+        
+        NSString *decodedExample3 = [cipher decode:@"zlha" offset: 7];
+        NSLog(@"%@", decodedExample3);
+        
+        
+        [cipher codeBreaker:@"okmg" with:@"tprl"];
+        
+        [cipher codeBreaker:@"tprl" with:@"zlha"];
+        
+        
+        
     }
+    
 }
